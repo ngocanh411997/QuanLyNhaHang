d283b23deb2b9f539479aed771452d9b31b0d65f
NgocAnh <ngocanh411997@gmail.com>
2018-03-20 23:16:04 +0700

    form

---
+++ QuanLyNhaHang/QuanLyNhaHang/view/HoaDonNhapKho.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaHang.view
+{
+    public partial class HoaDonNhapKho : UserControl
+    {
+        public HoaDonNhapKho()
+        {
+            InitializeComponent();
+        }
+    }
+}
